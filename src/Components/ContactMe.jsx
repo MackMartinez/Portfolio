83d44e013269a5cde398f46dc5c7eb4fd9166490
MackyClap <martimac2320@gmail.com>
2023-03-31 20:29:21 -0400

    add boilerplate got contact me

---
+++ src/Components/ContactMe.jsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+
+const ContactMe = () => {
+  return (
+    <Box>
+      <Typography>Contact</Typography>
+      <Typography>Interested in working together? Let's connect via email or LinkedIn</Typography>
+    </Box>
+  );
+}
+
+export default ContactMe;
