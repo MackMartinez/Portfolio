12e069ce45d6df59ba7927e646dd0c3c3ccfa3a9
MackyClap <martimac2320@gmail.com>
2023-04-01 13:21:01 -0400

    add hashlink to contact section

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -1,48 +1,67 @@
-import { Box, Typography } from '@mui/material';
-import emailjs from '@emailjs/browser';
-import { useRef } from 'react';
+import { Box, Typography } from "@mui/material";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 import { useForm } from "react-hook-form";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
 const ContactMe = () => {
-  const { register, handleSubmit, reset, formState: { errors } } = useForm();
-  const form = useRef()
-  
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const form = useRef();
+
   const sendEmail = (event) => {
     // event.preventDefault()
-    console.log("event", event)
-    
-    emailjs.sendForm('service_1bntywt', 'template_oagiruy', form.current, 'OthNt0rxupRmQEyDm')
-    .then(() => {
-          reset()
-      }, (error) => {
+    console.log("event", event);
+
+    emailjs
+      .sendForm(
+        "service_1bntywt",
+        "template_oagiruy",
+        form.current,
+        "OthNt0rxupRmQEyDm"
+      )
+      .then(
+        () => {
+          reset();
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
 
   // console.log(watch("example")); // watch input value by passing the name of it
   return (
-    <Box>
-      <Typography>Contact</Typography>
-      <Typography>Interested in working together? Let's connect via email or LinkedIn</Typography>
-    {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
-      <form ref={form} onSubmit={handleSubmit(sendEmail)}>
-        <Typography variant='label'>Name</Typography>
-        <input {...register("from_name")} />
-        
-        <Typography>Email</Typography>
-        {/* include validation with required or other standard HTML validation rules */}
-        <input {...register("from_email")} />
+    <Grid2 container={true} justifyContent={"center"} id="Contact">
+      <Box>
+        <Typography>Contact</Typography>
+        <Typography>
+          Interested in working together? Let's connect via email or LinkedIn
+        </Typography>
+        {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+        <form ref={form} onSubmit={handleSubmit(sendEmail)}>
+          <Typography variant="label">Name</Typography>
+          <input {...register("from_name")} />
 
-        <Typography>Message</Typography>
-        {/* include validation with required or other standard HTML validation rules */}
-        <input {...register("message")} />
-        {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <span>This field is required</span>}
-        
-        <input type="submit" />
-      </form>
-    </Box>
+          <Typography>Email</Typography>
+          {/* include validation with required or other standard HTML validation rules */}
+          <input {...register("from_email")} />
+
+          <Typography>Message</Typography>
+          {/* include validation with required or other standard HTML validation rules */}
+          <input {...register("message")} />
+          {/* errors will return when field validation fails  */}
+          {errors.exampleRequired && <span>This field is required</span>}
+
+          <input type="submit" />
+        </form>
+      </Box>
+    </Grid2>
   );
-}
+};
 
 export default ContactMe;
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -7,23 +7,24 @@ import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 const Landing = () => {
   return (
     <Grid2 container={true} justifyContent={"center"} id="Home">
-
-    <Box sx={{
-      pt:40,
-      pb:80
-    }}>
-      <Typography variant="h1">Macky Martinez</Typography>
-      <Typography variant="h3">Full-Stack Software Developer</Typography>
-      <Box sx={{pt: 4}}>
-      <Stack direction="row" spacing={2}>
-        <Button variant="outlined">About Me</Button>
-        <Button variant="outlined" href="#outlined-buttons">
-          Contact Me
-        </Button>
-      </Stack>
+      <Box
+        sx={{
+          pt: 40,
+          pb: 80,
+        }}
+      >
+        <Typography variant="h1">Macky Martinez</Typography>
+        <Typography variant="h3">Full-Stack Software Developer</Typography>
+        <Box sx={{ pt: 4 }}>
+          <Stack direction="row" spacing={2}>
+            <Button variant="outlined">About Me</Button>
+            <Button variant="outlined" href="#outlined-buttons">
+              Contact Me
+            </Button>
+          </Stack>
+        </Box>
       </Box>
-    </Box>
-      </Grid2>
+    </Grid2>
   );
 };
 
