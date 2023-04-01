869deae17a9feff9419bc21b0f024acba80428be
MackyClap <martimac2320@gmail.com>
2023-03-31 22:23:28 -0400

    check that e-mail is being received with correct data and form is being reset

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -1,39 +1,46 @@
 import { Box, Typography } from '@mui/material';
-import React from 'react';
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 import { useForm } from "react-hook-form";
 
 const ContactMe = () => {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = (event) => {
-    event.preventDefault()
-  }
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const form = useRef()
+  
+  const sendEmail = (event) => {
+    // event.preventDefault()
+    console.log("event", event)
+    
+    emailjs.sendForm('service_1bntywt', 'template_oagiruy', form.current, 'OthNt0rxupRmQEyDm')
+    .then(() => {
+          reset()
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 
-  console.log(watch("example")); // watch input value by passing the name of it
+  // console.log(watch("example")); // watch input value by passing the name of it
   return (
     <Box>
       <Typography>Contact</Typography>
       <Typography>Interested in working together? Let's connect via email or LinkedIn</Typography>
     {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Typography>Name</Typography>
-      <input {...register("name"), { required: true}} />
-      
-      <Typography>Email</Typography>
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("email", { required: true })} />
+      <form ref={form} onSubmit={handleSubmit(sendEmail)}>
+        <Typography variant='label'>Name</Typography>
+        <input {...register("from_name")} />
+        
+        <Typography>Email</Typography>
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("from_email")} />
 
-      <Typography>Subject</Typography>
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("subject", { required: true })} />
-
-      <Typography>Message</Typography>
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("message", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-      
-      <input type="submit" />
-    </form>
+        <Typography>Message</Typography>
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("message")} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+        
+        <input type="submit" />
+      </form>
     </Box>
   );
 }
