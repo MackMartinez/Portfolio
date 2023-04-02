a9ed11de3f40e299529ff688d1e48a5ad58f32a0
MackyClap <martimac2320@gmail.com>
2023-04-02 09:19:49 -0400

    add card for projects to display data

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,17 +1,56 @@
-import React from 'react';
+import React from "react";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import HabtrackLanding from '../Images/HabtrackLanding.JPG'
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const Projects = () => {
-
-  const projectData = ["HabitTracker","Jungle","Food-Pickup"]; 
+  const projectData = [
+    {
+      name: "HabTrack",
+      img: `${HabtrackLanding}`,
+      description:
+        "A habit tracking application that helps you monitor your progress towards developinga nd mainting positive habits. This was a collaborative full stack project with full CRUD capabilities. ",
+    },
+    { name: "Scheduler",
+      img: "test",
+      description: "React based project to create meetings for students to connect with mentors in real-time."},
+    { name: "Food-Pickup",
+    img: "test",
+    description: "Collaborative full stack project to simulate a restaurant ordering system for a customer to preorder and pickup their food. "},
+  ];
 
   return (
     <Grid2 container={true} justifyContent={"center"} id="Projects" pt={50}>
       {projectData.map((item, index) => (
-        <div key={index}>{item}</div>
+        <Card key={index} sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          alt="green iguana"
+          height="140"
+          image={require(`${HabtrackLanding}`)}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {item.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {item.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
       ))}
     </Grid2>
   );
-}
+};
 
 export default Projects;
