52b5e23f526d6319284a48aa812c5b5632b3ea97
MackyClap <martimac2320@gmail.com>
2023-04-03 20:54:49 -0400

    fix layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import { Container } from "@mui/system";
 import "./App.css";
 import DrawerAppBar from "./Components/Appbar";
@@ -7,22 +7,24 @@ import Projects from "./Components/Projects";
 import ContactMe from "./Components/ContactMe";
 import Skills from "./Components/Skills";
 import { BrowserRouter } from "react-router-dom";
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
-
 
 function App() {
+
   return (
     <BrowserRouter>
       <Box>
         <DrawerAppBar />
-          <Container maxWidth="md" sx={{ justifyContent: 'space-between', marginLeft:50}}>
+          <Container maxWidth="md" sx={{marginLeft:50}}>
             <Landing />
           </Container>
-          <Container maxWidth="1" sx={{ justifyContent: 'space-between'}}>
+          <Grid container={true} direction={"column"} justifyContent={"center"} pl={40} pr={40}>
+
             <Skills />
             <Projects />
             <ContactMe />
-          </Container>
+          </Grid>
+          {/* <Container sx={{ justifyContent: 'space-between', backgroundColor: "red"}}> */}
+          {/* </Container> */}
       </Box>
     </BrowserRouter>
   );
