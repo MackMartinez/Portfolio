65417803aff2c15ebf6a4512d56e9cf5d884f9cf
MackyClap <martimac2320@gmail.com>
2023-04-09 22:15:33 -0400

    write short about section

---
+++ src/Components/About.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material"
+import React from "react"
+
+function About() {
+  return (
+    <>
+    <Typography variant="h1" color="white">ABOUT</Typography>
+    <Typography>As a full stack developer with a background in financial accounting, I have the real world business experience necessary to succeed in a challenging and dynamic developer role. My technical skills include Javascript, React and Ruby. </Typography>
+    </>
+  )
+}
+
+export default About
